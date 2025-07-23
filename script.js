64b685f5c97b4764453ac50ac121da10aac1e2a6
Aditya Saxena <144054604+Adityasaxena1@users.noreply.github.com>
2025-07-23 20:27:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+window.onscroll = function () {
+  scrollingFunction();
+};
+
+function scrollingFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.querySelector(".progress-bar").style.width = scrolled + "%";
+}
